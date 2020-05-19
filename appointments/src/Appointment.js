8c004d9cd5797975f9dbba0ad12bf72c8422f64d
Steve Steinitz <steve@dtctc.com>
2020-05-19 18:00:55 +1000

    rendered appointments in ol

--- appointments/src/Appointment.js
+++ appointments/src/Appointment.js
@@ -4,4 +4,11 @@ export const Appointment = ({ customer }) => (
   <div>{customer.firstName}</div>
 );
 
-export const AppointmentsDayView = () => <div id="appointmentsDayView"></div>;
+export const AppointmentsDayView = ({appointments}) =>
+  <div id="appointmentsDayView">
+    <ol>
+      {appointments.map((appointment) => (
+        <div key={appointment.startsAt}/>
+      ))}
+    </ol>
+  </div>;
