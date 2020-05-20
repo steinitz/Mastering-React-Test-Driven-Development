e4105c0294cafc2af417eab4c9b05940ea29b6e5
Steve Steinitz <steve@dtctc.com>
2020-05-20 18:53:36 +1000

    Formatted appointment times each into li. Next - go to page 46 Selecting data to view

--- appointments/test/Appointment.test.js
+++ appointments/test/Appointment.test.js
@@ -37,6 +37,12 @@ describe('AppoiontmentsDayView', () => {
   const render = component =>
     ReactDOM.render(component, container);
 
+  const today = new Date();
+  const appointments = [
+    {startsAt: today.setHours(12, 0)},
+    {startsAt: today.setHours(13, 0)},
+  ];
+
   beforeEach(() => {
     container = document.createElement('div');
   });
@@ -47,16 +53,22 @@ describe('AppoiontmentsDayView', () => {
   })
 
   it('renders multiple appointments in an ol element', () => {
-    const today = new Date();
-    const appointments = [
-      {startsAt: today.setHours(12, 0)},
-      {startsAt: today.setHours(13, 0)},
-    ];
     render(<AppointmentsDayView appointments={appointments} />);
     expect(container.querySelector('ol')).not.toBeNull();
     expect(
       container.querySelector('ol').children
     ).toHaveLength(2);
+  })
+
+  it('renders each appointment in an li', () => {
+    render(<AppointmentsDayView appointments={appointments} />);
+    expect(container.querySelectorAll('li')).toHaveLength(2);
+    expect(
+      container.querySelectorAll('li')[0].textContent
+    ).toEqual('12:00');
+    expect(
+      container.querySelectorAll('li')[1].textContent
+    ).toEqual('13:00');
   })
 
 });
