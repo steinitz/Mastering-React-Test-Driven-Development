cf9873b85f486cf8b0759b8e2f8e80bf0c19f84b
Steve Steinitz <steve@dtctc.com>
2020-05-23 17:09:16 +1000

    Make the buttons select an appointment.  Next - Manually testing our changes, go to page 52.

--- appointments/test/Appointment.test.js
+++ appointments/test/Appointment.test.js
@@ -100,7 +100,7 @@ describe('AppoiontmentsDayView', () => {
     ).toEqual('button');
   });
 
-  it.skip('renders another appointment when selected', () => {
+  it('renders another appointment when selected', () => {
     render(<AppointmentsDayView appointments={appointments} />);
     const button = container.querySelectorAll('button')[1];
     ReactTestUtils.Simulate.click(button);
