00bb398fc4e75fa39978ecef7dcf0d6055b53c8a
Steve Steinitz <steve@dtctc.com>
2020-06-05 18:56:25 +1000

    Chapter 1 exercises: use css grid to set timeslots and appointment details side by side.  Next - Exercises, go to page 57.

--- appointments/src/Appointment.js
+++ appointments/src/Appointment.js
@@ -52,28 +52,32 @@ export const AppointmentsDayView = ({appointments}) => {
     setSelectedAppointment
   ] = useState(0);
   return (
-    <div id="appointmentsDayView">
-      <ol>
-        {appointments.map((appointment, i) => (
-          <li key={appointment.startsAt}>
-            <button
-              type="button"
-              onClick={() => setSelectedAppointment(i)}
-            >
-              {appointmentTimeOfDay(appointment.startsAt)}
-            </button>
-          </li>
-        ))}
-      </ol>
-      {
-        appointments.length === 0 ? (
-          <p>There are no appointments scheduled for today.</p>
-        ) : (
-          <Appointment appointment = {
-            appointments[selectedAppointment]
-          } />
-        )
-      }
+    <div className="appointment-day-layout" >
+      <div>
+        <ol>
+          {appointments.map((appointment, i) => (
+            <li key={appointment.startsAt}>
+              <button
+                type="button"
+                onClick={() => setSelectedAppointment(i)}
+              >
+                {appointmentTimeOfDay(appointment.startsAt)}
+              </button>
+            </li>
+          ))}
+        </ol>
+      </div>
+      <div id="appointmentsDayView">
+        {
+          appointments.length === 0 ? (
+            <p>There are no appointments scheduled for today.</p>
+          ) : (
+            <Appointment appointment = {
+              appointments[selectedAppointment]
+            } />
+          )
+        }
+      </div>
     </div>
   );
 };
