ade4c733b072b7a78604fd5667d41b678a94265d
Steve Steinitz <steve@dtctc.com>
2020-06-11 18:52:51 +1000

    Chapter 2: refactor test, add firstName field, test - Next - Extracting an expectation group function - go to page 64.

--- appointments/src/CustomerForm.js
+++ appointments/src/CustomerForm.js
@@ -1,3 +1,11 @@
 import React from 'react';
 
-export const CustomerForm = () => <form id="customer" />;
+export const CustomerForm = () => (
+  <form id="customer">
+    <input
+      type="text"
+      name="firstName"
+    />
+  </form>
+);
+
--- appointments/test/CustomerForm.test.js
+++ appointments/test/CustomerForm.test.js
@@ -9,10 +9,18 @@ describe('CustomerForm', () => {
     ({render, container} = createContainer());
   });
 
+  const form = id => container.querySelector(`form[id="${id}"`);
+
   it('renders a form', () => {
     render(<CustomerForm />);
-    expect(
-      container.querySelector('form[id="customer"]')
-    ).not.toBeNull();
+    expect(form('customer')).not.toBeNull();
   });
+
+  it('renders the first name field as a text box', () => {
+    render(<CustomerForm />);
+    const field = form('customer').elements.firstName;
+    expect(field).not.toBeNull();
+    expect(field.tagName).toEqual('INPUT');
+    expect(field.type).toEqual('text');
+  })
 });
