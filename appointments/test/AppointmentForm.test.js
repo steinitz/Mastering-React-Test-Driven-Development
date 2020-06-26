53c310da9c9275a9c7b393d5c25397844f234b8f
Steve Steinitz <steve@dtctc.com>
2020-06-26 23:25:02 +1000

    Chapter 2: added AppointmentForm and test.   Next - step 3 of Selecting from a dropdown - go to page 81

---
+++ appointments/test/AppointmentForm.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {createContainer} from './domManipulators';
+import {AppointmentForm} from '../src/AppointmentForm';
+
+describe ('AppointmentForm', () => {
+  let render, container;
+
+  beforeEach(() => {
+    ({render, container} = createContainer());
+  });
+
+  const form = id => container.querySelector(`form[id="${id}"]`);
+
+  it('renders a form', () => {
+    render(<AppointmentForm />);
+    expect(form('appointment')).not.toBeNull();
+  });
+});
