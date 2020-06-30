55d9a5d670100dcfb533179c124131fd218f43ec
Steve Steinitz <steve@dtctc.com>
2020-06-30 22:27:41 +1000

    Chapter 2: added AppointmentForm select service options and tests.   Next - Pre-selecting a value - go to page 85

--- appointments/src/AppointmentForm.js
+++ appointments/src/AppointmentForm.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
-export const AppointmentForm = () => (
+export const AppointmentForm = ({selectableServices}) => (
   <form id="appointment">
-    <select name="service" />
+    <select name="service">
+      <option />
+      {
+        selectableServices.map(s => (
+          <option key={s}>{s}</option>
+        ))
+      }
+    </select>
   </form>
 );
+
+AppointmentForm.defaultProps = {
+  selectableServices: [
+    'Cut',
+    'Blow-Dry',
+    'Cut & color',
+    'Beard trim',
+    'Cut & beard trim',
+    'Extensions'
+  ]
+}
