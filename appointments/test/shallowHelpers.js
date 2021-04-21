a0b54aac024c4c6be04fabfb58c5a5209706adbc
Steve Steinitz <steve@stzdev.com>
2021-04-21 20:58:34 +1000

    Create shallow renderer helper: list element children. Next: encapuslate render output to DRY tests - pg 167

---
+++ appointments/test/shallowHelpers.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export const childrenOf = element => {
+  if (typeof(element) === 'string') {
+    return [];
+  }
+  const {props: {children}} = element;
+  if (!children) {
+    return [];
+  }
+  if (typeof(children) === 'string') {
+    return [children];
+  }
+  if (Array.isArray(children)) {
+    return children;
+  }
+  return [children];
+}
---
+++ appointments/test/shallowHelpers.test.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {childrenOf} from './shallowHelpers';
+
+describe(childrenOf, () => {
+  it('returns no children', () => {
+    expect(childrenOf(<div />)).toEqual([]);
+  });
+
+    it('returns direct children', () => {
+    expect (
+      childrenOf(
+        <div>
+          <p>A</p>
+          <p>B</p>
+        </div>
+      )
+    ).toEqual([<p>A</p>, <p>B</p>]);
+  });
+
+  it('returns text as an array of one item', () => {
+    expect(childrenOf(<div>text</div>)).toEqual(['text']);
+  });
+
+  it('returns no children for text', () => {
+    expect(childrenOf('text')).toEqual([])
+  });
+
+  it('returns array of children for elements with one child', () => {
+    expect(
+      childrenOf(
+        <div>
+          <p>A</p>
+        </div>
+      )
+    ).toEqual([<p>A</p>]);
+  })
+
+});
